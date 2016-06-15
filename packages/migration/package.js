7464e3815b4de58b00e62ab42ff50a21af32b926
Jeremy Chen <convexset@gmail.com>
2016-06-15 16:42:46 +0800

    use underscore from npm at the project level

--- packages/migration/package.js
+++ packages/migration/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'convexset:migration',
-	version: '0.1.1_2',
+	version: '0.1.1_3',
 	summary: 'Simple API for data migration on reload (features \"template-level\" set-up/tear-down)',
 	git: 'https://github.com/convexset/meteor-migration',
 	documentation: '../../README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
 	api.versionsFrom('1.3.1');
 	api.use([
-		'ecmascript', 'underscore', 'ejson',
+		'ecmascript', 'ejson',
 		'reload',
 		'tmeasday:check-npm-versions@0.3.1'
 	], 'client');
